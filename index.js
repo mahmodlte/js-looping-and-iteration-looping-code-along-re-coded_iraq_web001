34f19eb297645410bd1661b0fa8278a5ce8fe89b
Mahmoud Mansour <mahmodlte@gmail.com>
2020-05-14 06:41:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+function writeCards (name, present) {
+
+  for (let i = 0; i < name.length; i++){
+return [`Thank you, ${name[i]}, for the wonderful ${present} gift!`];   
+    
+  }
+
+ 
+}
+writeCards(['Lisa', 'Kaitlin', 'Jan'], 'surprise');
