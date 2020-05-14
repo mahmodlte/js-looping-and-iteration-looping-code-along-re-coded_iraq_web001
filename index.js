e02e9e5c54e3601d5f9a4dfff3a6ddc79b221c01
Mahmoud Mansour <mahmodlte@gmail.com>
2020-05-14 06:41:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 function writeCards (name, present) {
+let arr = [];
+let msg ;
 
   for (let i = 0; i < name.length; i++){
-return [`Thank you, ${name[i]}, for the wonderful ${present} gift!`];   
-    
+msg = `Thank you, ${name[i]}, for the wonderful ${present} gift!`;
+
+arr.push(msg);
+
   }
 
- 
+return arr;
 }
-writeCards(['Lisa', 'Kaitlin', 'Jan'], 'surprise');
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
